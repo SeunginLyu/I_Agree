0821e927222e6086ed6e9410d2a1fa7559cdb54c
SeunginLyu <seungin.lyu@students.olin.edu>
2018-10-29 21:49:39 -0400

    Add second term

--- assets/js/display.js
+++ assets/js/display.js
@@ -1,9 +1,59 @@
-var removed = [];
+var removed = {};
 
 $( "#d1" ).click(function() {
   $("#display1").css("display", "block");
   $([document.documentElement, document.body]).animate({
         scrollTop: $("#display1").offset().top
+    }, 1500);
+  $( function() {
+      var progressbar = $( "#progressbar" ),
+        progressLabel = $( ".progress-label" );
+
+      progressbar.progressbar({
+        value: false,
+        change: function() {
+          progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+        },
+        complete: function() {
+          progressLabel.text( "Services Removed" );
+          $(".displaytext").text("");
+        }
+      });
+
+      function progress() {
+        var val = progressbar.progressbar( "value" ) || 0;
+
+        progressbar.progressbar( "value", val + 2 );
+
+        if ( val < 99 ) {
+          setTimeout( progress, 80 );
+        }
+      }
+      removed['Facebook'] = true;
+      removed['Twitter'] = true;
+      removed['Instagram'] = true;
+      removed['Google'] = true;
+      removed['LinkedIn'] = true;
+      removed['YouTube'] = true;
+      removed['Reddit'] = true;
+      removed['Amazon'] = true;
+      removed['Ebay'] = true;
+      removed['Slack'] = true;
+      setTimeout( progress, 2000 );
+    });
+});
+$("#n1").click(function(){
+  $([document.documentElement, document.body]).animate({
+        scrollTop: $("#2").offset().top
+    }, 2000);
+});
+
+var removed = [];
+
+$( "#d2" ).click(function() {
+  $("#display2").css("display", "block");
+  $([document.documentElement, document.body]).animate({
+        scrollTop: $("#display2").offset().top
     }, 2000);
   $( function() {
       var progressbar = $( "#progressbar" ),
@@ -29,7 +79,16 @@ $( "#d1" ).click(function() {
           setTimeout( progress, 80 );
         }
       }
-
+      removed['Facebook'] = true;
+      removed['Twitter'] = true;
+      removed['LinkedIn'] = true;
+      removed['YouTube'] = true;
+      removed['Reddit'] = true;
       setTimeout( progress, 2000 );
     });
+});
+$("#n2").click(function(){
+  $([document.documentElement, document.body]).animate({
+        scrollTop: $("#3").offset().top
+    }, 1500);
 });
